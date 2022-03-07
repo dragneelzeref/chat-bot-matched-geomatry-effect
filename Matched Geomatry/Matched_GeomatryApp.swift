1f787eb281fc630809f766c81980d052d5ff701a
Ravi Pansuriya <pansuriyaravi.rc@gmail.com>
2022-03-07 23:40:43 +0530

    Initial Commit

---
+++ Matched Geomatry/Matched_GeomatryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matched_GeomatryApp.swift
+//  Matched Geomatry
+//
+//  Created by Ravi on 07/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Matched_GeomatryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
